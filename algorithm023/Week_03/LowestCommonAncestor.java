d48b1e7c9e26270caf2bdd73868286db5b6ca0ba
xufei <986961902@qq.com>
2021-01-17 16:31:19 +0800

    week_03

---
+++ algorithm023/Week_03/LowestCommonAncestor.java
@@ -0,0 +1,16 @@
+package com.company.leetcode;
+
+/**
+ * @author
+ * @Description:给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ */
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == root || q == root) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+}
